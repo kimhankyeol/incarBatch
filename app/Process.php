e582dc58d9cf5d7ee39f650eb857c410cd53e418
kimhankyeol <kimhk1030@naver.com>
2020-04-09 13:54:34 +0900

    2020 04 09 13 54 modify

--- app/Process.php
+++ app/Process.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Process extends Model
 {
     //잡 테이블 기본설정
-    protected $table = 'Process';
-    protected $primaryKey = 'p_seq';
+    protected $table = 'OnlineBatch_Process';
+    protected $primaryKey = 'P_Seq';
 }
