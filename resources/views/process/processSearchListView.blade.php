5a486c6b785e6414cda3777d2cef9e6fdf5da05d
soooyeonlee <bogogae@naver.com>
2020-04-21 17:24:22 +0900

    2020-04-21 5:23pm processSearchListView modified

--- resources/views/process/processSearchListView.blade.php
+++ resources/views/process/processSearchListView.blade.php
@@ -3,8 +3,8 @@
     <td>{{'pro'.$proSc->P_WorkLargeCtg.'_'.$proSc->P_WorkMediumCtg.'_'.$proSc->P_Seq}}</td>
     <td>{{$proSc->P_WorkLargeName}}</td>
     <td>{{$proSc->P_WorkMediumName}}</td>
-    <td>{{$proSc->P_WorkMediumName}}</td>
-    <td>{{$proSc->P_FileName}}</td>
+    <td>{{$proSc->P_File}}</td>
+    <td>{{$proSc->P_Name}}</td>
     <td>{{$proSc->P_Sulmyung}}</td>
     <td>{{$proSc->P_RegId}}</td>
     <td>{{$proSc->P_RegDate}}</td>
