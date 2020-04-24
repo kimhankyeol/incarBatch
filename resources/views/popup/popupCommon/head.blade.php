445b0e15d0f919974b64998f6239458436963338
siyul-jh <gihum@naver.com>
2020-04-24 17:27:33 +0900

    2020-04-24 17:27 update

--- resources/views/popup/popupCommon/head.blade.php
+++ resources/views/popup/popupCommon/head.blade.php
@@ -1,13 +1,14 @@
-
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link href="/css/sb-admin-2.min.css" rel="stylesheet">
-    <link href="/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link href="/css/custom.css" rel="stylesheet">
-     <script type="text/javascript" src="/js/sortable.js"></script>
-    <?php
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <link href="/css/sb-admin-2.min.css" rel="stylesheet">
+  <link href="/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="/css/custom.css" rel="stylesheet">
+  <script type="text/javascript" src="/js/sortable.js"></script>
+  <script type="text/javascript" src="/js/jobJS/codeFunc.js"></script>
+  <script type="text/javascript" src="/js/jobJS/popupFunc.js"></script>
+  <?php
     $titleList = array(
         array('title' => '프로그램 상세', 'url' => '/popup/processInfo'),
         array('title' => '잡-프로그램 구성', 'url' => '/popup/jobGusung')
